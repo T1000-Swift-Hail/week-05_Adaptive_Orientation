c4e35b5a033cac0d5db90bdc5b4d7aa95babd031
Njoud Alrshidi <nujud.abdulaziz.has.s@tuwaiq.edu.sam>
2021-11-04 13:09:45 +0300

    done

---
+++ HW_Adaptive_Orientation/HW_Adaptive_Orientation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_Adaptive_Orientation
+//
+//  Created by Njoud Alrshidi on 29/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
